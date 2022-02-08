d9012591be21c57dd9c92ae2bd6235ceef0d1b32
Israel Andrade <israel.silva_1011@hotmail.com>
2022-02-08 09:08:57 -0300

    axios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const request = require('request');
+const axios = require('axios');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -11,11 +11,14 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
-  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body.url);
-    console.log(body.explanation);
-  });
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(response => {
+      console.log(response.data.url);
+      console.log(response.data.explanation);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
